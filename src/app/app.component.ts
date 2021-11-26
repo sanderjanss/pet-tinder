5a87ff63c1ceb788e23b7b29c3edd4ce601e2eb3
Sander Janssens <sander.janssens@outlook.be>
2021-11-26 19:34:35 +0100

    Added extra footer, header and layout components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pet-tinder';
+  title2 = `Petinder :moneybag: :moneybag: :moneybag:`;
 }
---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  private _year:number;
+
+  constructor() {
+    this._year = new Date().getFullYear()
+  }
+
+  get year(): number {
+    return this._year;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
